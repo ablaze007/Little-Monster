d0f26e68f163dc52c8701895b989085d533ef1d9
Bhuwan KC <alsatianbhuwan7@gmail.com>
2019-03-18 16:36:31 -0500

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Handlers
+    private Rigidbody2D _rigid;
+
+    [SerializeField]
+    private float jumpforce = 2.0f;
+    [SerializeField]
+    private float jumpResetTime = 0.25f;
+    private bool isJumping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigid = this.GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          
+    }
+
+    public void jump()
+    {
+        if (isJumping)
+            return;
+        isJumping = true;
+        _rigid.velocity = new Vector2(_rigid.velocity.x, jumpforce);
+        StartCoroutine(JumpResetRoutine());
+    }
+
+    IEnumerator JumpResetRoutine()
+    {
+        yield return new WaitForSeconds(jumpResetTime);
+        isJumping = false;
+    }
+
+}
